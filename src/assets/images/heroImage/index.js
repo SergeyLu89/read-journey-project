27aa570bffda323db9a53c9c4402bb45d34fa904
Sergey Lutsenko <lytsenko.89@gmail.com>
2024-04-29 16:47:49 +0300

    added hero img, retina display img, WebP

---
+++ src/assets/images/heroImage/index.js
@@ -0,0 +1,19 @@
+import heroMobilePng from './hero-mobile.png';
+import heroMobilePng2x from './hero-mobile@2x.png';
+import heroMobileWeb from './hero-mobile.webp';
+import heroMobileWeb2x from './hero-mobile@2x.webp';
+import heroDesktopPng from './hero-desktop.png';
+import heroDesktopPng2x from './hero-desktop@2x.png';
+import heroDesktopWeb from './hero-desktop.webp';
+import heroDesktopWeb2x from './hero-desktop@2x.webp';
+
+export {
+  heroMobilePng,
+  heroMobilePng2x,
+  heroMobileWeb,
+  heroMobileWeb2x,
+  heroDesktopPng,
+  heroDesktopPng2x,
+  heroDesktopWeb,
+  heroDesktopWeb2x,
+};
