01356d50ce904d9569a445511355b0889388a167
SergeyLu89 <130161910+SergeyLu89@users.noreply.github.com>
2024-04-30 12:55:06 +0000

    Deploying to gh-pages from @ SergeyLu89/read-journey-project@0933ee7541f286ef0fb417d4e513b6e9c77b9f34 🚀

---
+++ static/js/286.dd34ae18.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkread_journey_project=self.webpackChunkread_journey_project||[]).push([[286],{286:function(e,r,n){n.r(r);var u=n(184);r.default=function(){return(0,u.jsx)(u.Fragment,{children:"RECOMMENDED PAGE"})}}}]);
+//# sourceMappingURL=286.dd34ae18.chunk.js.map
---
+++ static/js/466.32801e3e.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkread_journey_project=self.webpackChunkread_journey_project||[]).push([[466],{466:function(e,r,n){n.r(r);var u=n(184);r.default=function(){return(0,u.jsx)(u.Fragment,{children:"READING PAGE"})}}}]);
+//# sourceMappingURL=466.32801e3e.chunk.js.map
