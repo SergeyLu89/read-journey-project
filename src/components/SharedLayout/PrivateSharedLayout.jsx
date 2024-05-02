b9fd241533cea1d1dfb7ac31cdafb03f1b4798da
Serhii Lutsenko <130161910+SergeyLu89@users.noreply.github.com>
2024-05-02 11:53:47 +0300

    fix Layouts

--- src/components/SharedLayout/PrivateSharedLayout.jsx
+++ src/components/SharedLayout/PrivateSharedLayout.jsx
@@ -1,18 +1,17 @@
+import Container from 'components/Container/Container';
+import Header from 'components/Header/Header';
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
-// import Header from 'components/Header/Header';
-import Container from '../Container/Container';
 
 const PrivateSharedLayout = () => {
   return (
-    <>
-      {/* <Header /> */}
-      <Container>
-        <Suspense fallback={'Loading...'}>
-          <Outlet />
-        </Suspense>
-      </Container>
-    </>
+    <Container>
+      <Header />
+
+      <Suspense fallback={'Loading...'}>
+        <Outlet />
+      </Suspense>
+    </Container>
   );
 };
 
