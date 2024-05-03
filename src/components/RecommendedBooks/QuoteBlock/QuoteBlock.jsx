eff75eaf982c9bf80e1e584cf33eeecbf2bcac1f
Serhii Lutsenko <130161910+SergeyLu89@users.noreply.github.com>
2024-05-03 16:31:11 +0300

    created Quote component

---
+++ src/components/RecommendedBooks/QuoteBlock/QuoteBlock.jsx
@@ -0,0 +1,14 @@
+// import css from './QuoteBlock.module.css'
+
+const QuoteBlock = () => {
+  return (
+    <div>
+      <div>SVG заглушка</div>
+      <p>
+        "Books are <span>windows</span> to the world, and reading is a journey
+        into the unknown."
+      </p>
+    </div>
+  );
+};
+export default QuoteBlock;
