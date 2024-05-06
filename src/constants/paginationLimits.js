fc1f1dc61aa872cd5fd3ed9770efabaeb3635852
Serhii Lutsenko <130161910+SergeyLu89@users.noreply.github.com>
2024-05-06 16:01:03 +0300

    logic for determining device type

---
+++ src/constants/paginationLimits.js
@@ -0,0 +1,9 @@
+import { DeviceTypes } from './deviceTypes';
+
+const RecommendedBooksLimit = {
+  [DeviceTypes.MOBILE]: 2,
+  [DeviceTypes.TABLET]: 8,
+  [DeviceTypes.DESKTOP]: 10,
+};
+
+export { RecommendedBooksLimit };
