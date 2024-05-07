ad747fd592e1ff9e114cd8939b4c44cf5cf49241
Serhii Lutsenko <130161910+SergeyLu89@users.noreply.github.com>
2024-05-07 09:44:59 +0300

    added logic for displaying information in components

--- src/components/RecommendedBooks/RecommendedBooksItem/RecommendedBooksItem.jsx
+++ src/components/RecommendedBooks/RecommendedBooksItem/RecommendedBooksItem.jsx
@@ -1,13 +1,37 @@
-// import css from './RecommendedBooksItem.module.css'
+import BookInfoComponent from 'components/reUseComponents/BookInfoComponent/BookInfoComponent';
+import css from './RecommendedBooksItem.module.css';
+
+import Modal from 'components/reUseComponents/Modal/Modal';
+import { useState } from 'react';
 
 const RecommendedBooksItem = ({ book }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const openModal = () => {
+    setIsOpen(true);
+  };
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+
   const { author, imageUrl, title } = book;
   return (
-    <li>
-      <img src={imageUrl} alt={title} />
-      <h4>{title}</h4>
-      <p>{author}</p>
-    </li>
+    <>
+      <li>
+        <button type="button" onClick={openModal} className={css.modalOpenBtn}>
+          <img src={imageUrl} alt={title} className={css.bookImage} />
+        </button>
+
+        <h4>{title}</h4>
+        <p>{author}</p>
+      </li>
+      {isOpen && (
+        <Modal isOpen={isOpen} closeFnc={closeModal}>
+          <BookInfoComponent book={book} />
+          <button type="button">Add to library</button>
+        </Modal>
+      )}
+    </>
   );
 };
 export default RecommendedBooksItem;
--- src/pages/RecommendedPage/RecommendedPage.jsx
+++ src/pages/RecommendedPage/RecommendedPage.jsx
@@ -4,18 +4,8 @@ import Functionality from 'components/RecommendedBooks/Functionality/Functionali
 import QuoteBlock from 'components/RecommendedBooks/QuoteBlock/QuoteBlock';
 import RecommendedBooksList from 'components/RecommendedBooks/RecommendedBooksList/RecommendedBooksList';
 import Dashboard from 'components/reUseComponents/Dashboard/Dashboard';
-// import { useEffect } from 'react';
-// import { useDispatch, useSelector } from 'react-redux';
-// import { getRecommended } from '../../redux/books/recommendedBooks/recommendedBooksOperations';
-// import { selectRecommendedBooks } from '../../redux/books/recommendedBooks/recommendedBooksSelectors';
 
 const RecommendedPage = () => {
-  // const books = useSelector(selectRecommendedBooks);
-  // const dispatch = useDispatch();
-
-  // useEffect(() => {
-  //   dispatch(getRecommended());
-  // }, [dispatch]);
   return (
     <>
       <Dashboard>
