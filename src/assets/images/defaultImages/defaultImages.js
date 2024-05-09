1a96eb63c32e83e5c97a20072fa3e47e19baa837
Serhii Lutsenko <130161910+SergeyLu89@users.noreply.github.com>
2024-05-09 10:24:25 +0300

    add default images

---
+++ src/assets/images/defaultImages/defaultImages.js
@@ -0,0 +1,4 @@
+import defaultImageBookSmallPng from './image-book-small.png';
+import defaultImageBookbigPng from './image-book-big.png';
+
+export { defaultImageBookSmallPng, defaultImageBookbigPng };
