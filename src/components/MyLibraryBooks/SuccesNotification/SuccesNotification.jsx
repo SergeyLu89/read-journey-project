f5aa81f08f0262f7810abdb314d1a9042ad1361d
Serhii Lutsenko <130161910+SergeyLu89@users.noreply.github.com>
2024-05-10 09:34:12 +0300

    created Notification Component

---
+++ src/components/MyLibraryBooks/SuccesNotification/SuccesNotification.jsx
@@ -0,0 +1,15 @@
+// import css from './SuccesNotification.module.css'
+
+const SuccesNotification = () => {
+  return (
+    <>
+      <span>👍</span>
+      <h3>Good job</h3>
+      <p>
+        Your book is now in <span>the library!</span> The joy knows no bounds
+        and now you can start your training
+      </p>
+    </>
+  );
+};
+export default SuccesNotification;
