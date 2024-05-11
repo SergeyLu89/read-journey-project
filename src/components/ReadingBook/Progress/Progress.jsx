1d3f237982142b7297043e7c46aef44655ff6b8c
Serhii Lutsenko <130161910+SergeyLu89@users.noreply.github.com>
2024-05-11 11:39:18 +0300

    created Progress component

---
+++ src/components/ReadingBook/Progress/Progress.jsx
@@ -0,0 +1,14 @@
+// import css from './Progress.module.css'
+const Progress = () => {
+  return (
+    <>
+      <h3>Progress</h3>
+      <p>
+        Here you will see when and how much you read. To record, click on the
+        button above.
+      </p>
+      <p>🌟</p>
+    </>
+  );
+};
+export default Progress;
