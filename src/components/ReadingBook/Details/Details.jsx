6d61b2242b4803a099545bb27b9e8e0acd1d2000
Serhii Lutsenko <130161910+SergeyLu89@users.noreply.github.com>
2024-05-12 18:40:13 +0300

    added buttons on Details component

--- src/components/ReadingBook/Details/Details.jsx
+++ src/components/ReadingBook/Details/Details.jsx
@@ -1,5 +1,34 @@
-// import css from './Details.module.css'
-const Details = ({ children }) => {
-  return <>{children}</>;
+import css from './Details.module.css';
+
+import { useState } from 'react';
+import Diary from '../Diary/Diary';
+import Statistics from '../Statistics/Statistics';
+
+const Details = ({ book }) => {
+  const [activeChild, setActiveChild] = useState('Diary');
+  // const { progress } = book;
+  // console.log('progress: ', progress);
+  return (
+    <>
+      <div className={css.navigationLinks}>
+        <button
+          onClick={() => setActiveChild('Diary')}
+          className={`${css.navLink} ${activeChild === 'Diary' && css.active}`}
+        >
+          Diary
+        </button>
+        <button
+          onClick={() => setActiveChild('Statistics')}
+          className={`${css.navLink} ${
+            activeChild === 'Statistics' && css.active
+          }`}
+        >
+          Statistics
+        </button>
+      </div>
+      {activeChild === 'Diary' && <Diary book={book} />}
+      {activeChild === 'Statistics' && <Statistics />}
+    </>
+  );
 };
 export default Details;
