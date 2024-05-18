eca023eb6028d884029d0bb6481e65b9077ffa64
Serhii Lutsenko <130161910+SergeyLu89@users.noreply.github.com>
2024-05-18 22:20:48 +0300

    added component and styles on page

--- src/pages/RegisterPage/RegisterPage.jsx
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -1,12 +1,14 @@
-// import css from './RegisterPage.module.css'
+import css from './RegisterPage.module.css';
 
 import RegisterForm from 'components/Forms/AuthForms/RegisterForm/RegisterForm';
+import HeroImageComponent from 'components/HeroImageComponent/HeroImageComponent';
 
 const RegisterPage = () => {
   return (
-    <>
+    <div className={css.pageWrapper}>
       <RegisterForm />
-    </>
+      <HeroImageComponent />
+    </div>
   );
 };
 
