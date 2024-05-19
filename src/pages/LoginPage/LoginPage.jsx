1def68da1ed326d47e45a64366097259a3a4f8c9
Serhii Lutsenko <130161910+SergeyLu89@users.noreply.github.com>
2024-05-19 22:02:05 +0300

    added styles in login page

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,12 +1,14 @@
-// import css from './LoginPage.module.css';
+import css from './LoginPage.module.css';
 
 import LoginForm from 'components/Forms/AuthForms/LoginForm/LoginForm';
+import HeroImageComponent from 'components/HeroImageComponent/HeroImageComponent';
 
 const LoginPage = () => {
   return (
-    <>
+    <div className={css.pageWrapper}>
       <LoginForm />
-    </>
+      <HeroImageComponent />
+    </div>
   );
 };
 export default LoginPage;
