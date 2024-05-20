871e0472813ccad5421f4077714892148eadef19
Serhii Lutsenko <130161910+SergeyLu89@users.noreply.github.com>
2024-05-20 18:33:08 +0300

    add styles in Functionality

--- src/components/RecommendedBooks/Functionality/Functionality.jsx
+++ src/components/RecommendedBooks/Functionality/Functionality.jsx
@@ -1,31 +1,43 @@
 import css from './Functionality.module.css';
+import sprite from '../../../assets/images/sprite.svg';
 
 import { Link } from 'react-router-dom';
 
 const Functionality = () => {
   return (
-    <>
+    <section className={css.functionalitySection}>
       <h3>Start your workout</h3>
-      <ul>
-        <li>
-          <div>1</div>
-          <p>
+      <ul className={css.functionalityList}>
+        <li className={css.functionalityListItem}>
+          <div className={css.numberWrapper}>
+            <span>1</span>
+          </div>
+          <p className={css.functionalityDescr}>
             <span>Create a personal library:</span> add the books you intend to
             read to it.
           </p>
         </li>
-        <li>
-          <div>2</div>
-          <p>
+        <li className={css.functionalityListItem}>
+          <div className={css.numberWrapper}>
+            <span>2</span>
+          </div>
+          <p className={css.functionalityDescr}>
             <span>Create your first workout:</span> define a goal, choose a
             period, start training.
           </p>
         </li>
       </ul>
-      <Link className={css.functionalityLink} to="/library">
-        My library
-      </Link>
-    </>
+      <div className={css.functionalityLinkWrapper}>
+        <Link className={css.functionalityLink} to="/library">
+          My library
+        </Link>
+        <Link className={css.functionalityLinkSvg} to="/library">
+          <svg width="24" height="24" aria-label="arrow icon">
+            <use href={sprite + '#icon-log-in'}></use>
+          </svg>
+        </Link>
+      </div>
+    </section>
   );
 };
 export default Functionality;
