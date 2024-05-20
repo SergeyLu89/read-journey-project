1a2a9ed2dfe59f440f66cee6620b50f3f73f7a36
Serhii Lutsenko <130161910+SergeyLu89@users.noreply.github.com>
2024-05-20 14:27:49 +0300

    fix logo

--- src/components/Header/Logo/Logo.jsx
+++ src/components/Header/Logo/Logo.jsx
@@ -6,16 +6,10 @@ const Logo = () => {
   return (
     <>
       <Link to="/" className={css.logo}>
-        <svg
-          width="42"
-          height="17"
-          fill="#1f1f1f"
-          stroke="#1f1f1f"
-          aria-label="logo icon"
-        >
+        <svg width="42" height="17" aria-label="logo icon">
           <use href={sprite + '#icon-logo'}></use>
         </svg>
-        read journey
+        <span>read journey</span>
       </Link>
     </>
   );
