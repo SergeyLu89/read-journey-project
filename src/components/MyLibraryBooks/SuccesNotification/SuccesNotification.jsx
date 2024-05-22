b024a32858d3ec6784f44310599b7ed861ad093b
SergeyLu89 <lytsenko.89@gmail.com>
2024-05-22 16:59:49 +0300

    added notification styles

--- src/components/MyLibraryBooks/SuccesNotification/SuccesNotification.jsx
+++ src/components/MyLibraryBooks/SuccesNotification/SuccesNotification.jsx
@@ -1,15 +1,17 @@
-// import css from './SuccesNotification.module.css'
+import css from './SuccesNotification.module.css';
+import { likePng } from '../../../assets/images/defaultImages/defaultImages';
 
 const SuccesNotification = () => {
   return (
-    <>
-      <span>👍</span>
+    <div className={css.notificationWrapper}>
+      <img src={likePng} alt="like" className={css.likeImage} />
       <h3>Good job</h3>
-      <p>
-        Your book is now in <span>the library!</span> The joy knows no bounds
-        and now you can start your training
+      <p className={css.notificationDescr}>
+        Your book is now in{' '}
+        <span className={css.notificationDescrAccent}>the library!</span> The
+        joy knows no bounds and now you can start your training
       </p>
-    </>
+    </div>
   );
 };
 export default SuccesNotification;
