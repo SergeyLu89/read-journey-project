15f3595c4e0e22fc8e85a1688f2d81752877813f
SergeyLu89 <lytsenko.89@gmail.com>
2024-05-22 16:36:00 +0300

    added default images

--- src/assets/images/defaultImages/defaultImages.js
+++ src/assets/images/defaultImages/defaultImages.js
@@ -1,5 +1,6 @@
 import defaultImageBookSmallPng from './image-book-small.png';
 import defaultImageBookbigPng from './image-book-big.png';
 import booksPng from './books.png';
+import likePng from './like.png';
 
-export { defaultImageBookSmallPng, defaultImageBookbigPng, booksPng };
+export { defaultImageBookSmallPng, defaultImageBookbigPng, booksPng, likePng };
