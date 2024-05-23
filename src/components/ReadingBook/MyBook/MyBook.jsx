083008e485f5ed04b27a3c043ee094a5a028b866
SergeyLu89 <lytsenko.89@gmail.com>
2024-05-23 11:10:14 +0300

    added styles in MyBook component

--- src/components/ReadingBook/MyBook/MyBook.jsx
+++ src/components/ReadingBook/MyBook/MyBook.jsx
@@ -1,5 +1,6 @@
-import { useSelector } from 'react-redux';
 import css from './MyBook.module.css';
+import sprite from '../../../assets/images/sprite.svg';
+import { useSelector } from 'react-redux';
 import { defaultImageBookbigPng } from 'assets/images/defaultImages/defaultImages';
 import { selectIsWhileReading } from '../../../redux/books/readingBook/readingBookSelectors';
 
@@ -7,17 +8,23 @@ const MyBook = ({ book }) => {
   const isWhileReading = useSelector(selectIsWhileReading);
   const { imageUrl, title, author } = book;
   return (
-    <section>
+    <section className={css.myBookSction}>
       <h2>My reading</h2>
-      <div>
+      <div className={css.bookWrapper}>
         <img src={imageUrl ? imageUrl : defaultImageBookbigPng} alt={title} />
         <h3>{title}</h3>
         <p>{author}</p>
       </div>
       {isWhileReading ? (
-        <span className={css.indicatorStop}>STOP</span>
+        // <span className={css.indicatorStop}>STOP</span>
+        <svg className={css.statusSvg} aria-label="stop icon">
+          <use href={sprite + '#icon-stop'}></use>
+        </svg>
       ) : (
-        <span className={css.indicatorStart}>START</span>
+        // <span className={css.indicatorStart}>START</span>
+        <svg className={css.statusSvg} aria-label="start icon">
+          <use href={sprite + '#icon-start'}></use>
+        </svg>
       )}
     </section>
   );
